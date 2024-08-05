4117de3824373aa35b8985342c9b54f48703dea7
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-05 16:35:36 +0700

    snap shot

--- src/main/java/com/codegym/quizappbackendmodule6/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.quizappbackendmodule6.repositories;
-
-import com.codegym.quizappbackendmodule6.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/com/codegym/quizappbackendmodule6/services/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.quizappbackendmodule6.services;
-
-import com.codegym.quizappbackendmodule6.model.User;
-
-import java.util.List;
-
-public interface IUserService {
-    List<User> getAllUsers();
-}
