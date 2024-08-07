3a0bf9b19a2a947a7b49d513438c1b8d682a09a3
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-07 18:59:54 +0700

    update lại câu lệnh HQL để không bị bug trong TeacherApprovalRepository.java khi tìm người dùng đang chờ phê duyệt

--- src/main/java/com/codegym/quizappbackendmodule6/repository/TeacherApprovalRepository.java
+++ src/main/java/com/codegym/quizappbackendmodule6/repository/TeacherApprovalRepository.java
@@ -8,20 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TeacherApprovalRepository extends JpaRepository<TeacherApproval,Long> {
-    @Query(value = "SELECT ta.id AS idTeacherApprovals, u.name AS userName, u.email AS userEmail, ta.status AS teacherApprovalsStatus, ta.approved_at AS approvedAt " +
-            "FROM teacher_approvals ta " +
-            "JOIN users u ON ta.user_id = u.id " +
-            "WHERE ta.status = :status",
-            nativeQuery = true)
+public interface TeacherApprovalRepository extends JpaRepository<TeacherApproval, Long> {
+    @Query(value = "SELECT ta.id AS idteacherapprovals, u.name AS username, u.email AS useremail, ta.status AS teacherapprovalsstatus, ta.approved_at AS approvedat " + "FROM teacher_approvals ta " + "JOIN users u ON ta.user_id = u.id " + "WHERE ta.status = :status", nativeQuery = true)
     List<TeacherApprovalDTO> findAllPending(String status);
 
-    @Query(value = "SELECT ta.id AS idTeacherApprovals, u.name AS userName, u.email AS userEmail, ta.status AS teacherApprovalsStatus, ta.approved_at AS approvedAt " +
-            "FROM teacher_approvals ta " +
-            "JOIN users u ON ta.user_id = u.id " +
-            "WHERE ta.status = :status",
-            nativeQuery = true)
+    @Query(value = "SELECT ta.id AS idteacherapprovals, u.name AS username, u.email AS useremail, ta.status AS teacherapprovalsstatus, ta.approved_at AS approvedat " + "FROM teacher_approvals ta " + "JOIN users u ON ta.user_id = u.id " + "WHERE ta.status = :status", nativeQuery = true)
     List<TeacherApprovalDTO> findAllApproval(String status);
-    @Query(value = "SELECT * FROM TeacherApproval t WHERE t.user_id = :userId" , nativeQuery = true)
-    TeacherApproval findByUserId(Long userId);
+
+    @Query("SELECT t FROM TeacherApproval t WHERE t.user.id = :userId")
+    TeacherApproval findByUserId(@Param("userId") Long userId);
+
 }
