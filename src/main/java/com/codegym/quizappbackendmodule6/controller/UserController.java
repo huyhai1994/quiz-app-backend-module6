c3660a0c6312702e02785429b1d7458412409c0c
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-08 23:22:06 +0700

    11h21 pull nhanh dev

--- src/main/java/com/codegym/quizappbackendmodule6/controller/UserController.java
+++ src/main/java/com/codegym/quizappbackendmodule6/controller/UserController.java
@@ -1,102 +1,102 @@
-//package com.codegym.quizappbackendmodule6.controller;
-//
-//import com.codegym.quizappbackendmodule6.configs.PasswordGenerator;
-//import com.codegym.quizappbackendmodule6.model.TeacherApproval;
-//import com.codegym.quizappbackendmodule6.model.User;
-//import com.codegym.quizappbackendmodule6.model.dto.*;
-//import com.codegym.quizappbackendmodule6.service.Impl.EmailService;
-//import com.codegym.quizappbackendmodule6.service.TeacherApprovalService;
-//import com.codegym.quizappbackendmodule6.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//@RequestMapping("/users")
-//@CrossOrigin("*")
-//@RequiredArgsConstructor
-//public class UserController {
-//    private final UserService userService;
-//    private final TeacherApprovalService teacherApprovalService;
-//    private final EmailService emailService;
-//
-//
-//    @GetMapping("/get-all")
-//    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
-//        List<User> users = userService.findAllUsers();
-//        List<UserResponseDTO> userResponseDTOs = users.stream().map(user -> new UserResponseDTO(user.getName(), user.getEmail(), user.getAvatar(), user.getRegisteredAt(), user.getLastLogin())).collect(Collectors.toList());
-//        return ResponseEntity.ok(userResponseDTOs);
-//    }
-//
-////    @GetMapping("/approval-pending")
-////    public ResponseEntity<List<UserWithApprovalsProjection>> getUsersWithApprovals() {
-////        List<UserWithApprovalsProjection> users = userService.findUsersWithApprovals();
-////        return ResponseEntity.ok(users);
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public ResponseEntity<Void> deleteUser(@RequestParam Long id) {
-//        userService.deleteUser(id);
-//        return ResponseEntity.noContent().build();
-//    }
-//
-//    @PostMapping("/request-teacher/{id}")
-//    public ResponseEntity<Void> requestTeacherRole(@PathVariable Long id) {
-////        userService.requestTeacherRole(id);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @GetMapping("/teachers")
-//    public ResponseEntity<List<TeacherResponseDTO>> getTeachers() {
-//        List<TeacherResponseDTO> teacherResponseDTOs = userService.findTeachers();
-//        if (teacherResponseDTOs.isEmpty()) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(teacherResponseDTOs);
-//    }
-//
-//    @GetMapping("/students")
-//    public ResponseEntity<List<StudentResponseDTO>> getStudents() {
-//        return ResponseEntity.ok(userService.findStudents());
-//    }
-//
-//    @PutMapping("/approval/{id}")
-//    public ResponseEntity<User> approveUser(@PathVariable Long id) {
-//        User user = userService.findById(id).orElseThrow(() -> new RuntimeException("User không tồn tại với ID: " + id));
-//        String randomPassword = PasswordGenerator.generateRandomPassword(10);
-//        userService.updateUserPassword(id, randomPassword);
-//        emailService.sendConfirmationEmail(user.getEmail(), "Confirmation Email", "Your account has been approved.", randomPassword);
-//        user.setRoleId(3);
-//        TeacherApproval teacherApproval = teacherApprovalService.findByUserId(id); // Assuming a
-//        teacherApproval.setApprovalStatus("APPROVED");
-//        teacherApproval.setApprovedAt(LocalDateTime.now());
-//        userService.save(user);
-//        return ResponseEntity.ok(user);
-//    }
-//
-//    @GetMapping("/search-teacher")
-//    public ResponseEntity<List<UserSearchResponseDTO>> getTeachersByNameAndEmail(@RequestParam String name, @RequestParam String email) {
-//        List<UserSearchResponseDTO> userSearchResponseDTOs = userService.findUsersByRolesAndNameOrEmail(3L, name, email);
-//        if (userSearchResponseDTOs.isEmpty()) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(userSearchResponseDTOs);
-//    }
-//
-//    @GetMapping("/search-student")
-//    public ResponseEntity<List<UserSearchResponseDTO>> getStudentsByNameAndEmail(@RequestParam String name, @RequestParam String email) {
-//        List<UserSearchResponseDTO> userSearchResponseDTOs = userService.findUsersByRolesAndNameOrEmail(2L, name, email);
-//        if (userSearchResponseDTOs.isEmpty()) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(userSearchResponseDTOs);
-//    }
-//
-//
-//}
-//
-//
+package com.codegym.quizappbackendmodule6.controller;
+
+import com.codegym.quizappbackendmodule6.configs.PasswordGenerator;
+import com.codegym.quizappbackendmodule6.model.TeacherApproval;
+import com.codegym.quizappbackendmodule6.model.User;
+import com.codegym.quizappbackendmodule6.model.dto.*;
+import com.codegym.quizappbackendmodule6.service.Impl.EmailService;
+import com.codegym.quizappbackendmodule6.service.TeacherApprovalService;
+import com.codegym.quizappbackendmodule6.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final TeacherApprovalService teacherApprovalService;
+    private final EmailService emailService;
+
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        List<UserResponseDTO> userResponseDTOs = users.stream().map(user -> new UserResponseDTO(user.getName(), user.getEmail(), user.getAvatar(), user.getRegisteredAt(), user.getLastLogin())).collect(Collectors.toList());
+        return ResponseEntity.ok(userResponseDTOs);
+    }
+
+    @GetMapping("/approval-pending")
+    public ResponseEntity<List<UserWithApprovalsProjection>> getUsersWithApprovals() {
+        List<UserWithApprovalsProjection> users = userService.findUsersWithApprovals();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@RequestParam Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/request-teacher/{id}")
+    public ResponseEntity<Void> requestTeacherRole(@PathVariable Long id) {
+        userService.requestTeacherRole(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/teachers")
+    public ResponseEntity<List<TeacherResponseDTO>> getTeachers() {
+        List<TeacherResponseDTO> teacherResponseDTOs = userService.findTeachers();
+        if (teacherResponseDTOs.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(teacherResponseDTOs);
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<StudentResponseDTO>> getStudents() {
+        return ResponseEntity.ok(userService.findStudents());
+    }
+
+    @PutMapping("/approval/{id}")
+    public ResponseEntity<User> approveUser(@PathVariable Long id) {
+        User user = userService.findById(id).orElseThrow(() -> new RuntimeException("User không tồn tại với ID: " + id));
+        String randomPassword = PasswordGenerator.generateRandomPassword(10);
+        userService.updateUserPassword(id, randomPassword);
+        emailService.sendConfirmationEmail(user.getEmail(), "Confirmation Email", "Your account has been approved.", randomPassword);
+        user.setRoleId(3);
+        TeacherApproval teacherApproval = teacherApprovalService.findByUserId(id); // Assuming a
+        teacherApproval.setApprovalStatus("APPROVED");
+        teacherApproval.setApprovedAt(LocalDateTime.now());
+        userService.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/search-teacher")
+    public ResponseEntity<List<UserSearchResponseDTO>> getTeachersByNameAndEmail(@RequestParam String name, @RequestParam String email) {
+        List<UserSearchResponseDTO> userSearchResponseDTOs = userService.findUsersByRolesAndNameOrEmail(3L, name, email);
+        if (userSearchResponseDTOs.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userSearchResponseDTOs);
+    }
+
+    @GetMapping("/search-student")
+    public ResponseEntity<List<UserSearchResponseDTO>> getStudentsByNameAndEmail(@RequestParam String name, @RequestParam String email) {
+        List<UserSearchResponseDTO> userSearchResponseDTOs = userService.findUsersByRolesAndNameOrEmail(2L, name, email);
+        if (userSearchResponseDTOs.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userSearchResponseDTOs);
+    }
+
+
+}
+
+
--- src/main/java/com/codegym/quizappbackendmodule6/service/Impl/UserServiceImpl.java
+++ src/main/java/com/codegym/quizappbackendmodule6/service/Impl/UserServiceImpl.java
@@ -1,99 +1,99 @@
-//package com.codegym.quizappbackendmodule6.service.Impl;
-//
-//import com.codegym.quizappbackendmodule6.model.TeacherApproval;
-//import com.codegym.quizappbackendmodule6.model.User;
-//import com.codegym.quizappbackendmodule6.model.dto.StudentResponseDTO;
-//import com.codegym.quizappbackendmodule6.model.dto.TeacherResponseDTO;
-//import com.codegym.quizappbackendmodule6.model.dto.UserSearchResponseDTO;
-//import com.codegym.quizappbackendmodule6.model.dto.UserWithApprovalsProjection;
-//import com.codegym.quizappbackendmodule6.repository.UserRepository;
-//import com.codegym.quizappbackendmodule6.service.TeacherApprovalService;
-//import com.codegym.quizappbackendmodule6.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//
-//@Service
-//@RequiredArgsConstructor
-//public class UserServiceImpl implements UserService {
-//    private final UserRepository userRepository;
-//    private final TeacherApprovalService teacherApprovalService;
-//
-//    @Override
-//    public List<User> findAllUsers() {
-//        return userRepository.findAll();
-//    }
-//
-//    @Override
-//    public List<UserWithApprovalsProjection> findUsersWithApprovals() {
-//        return userRepository.getUsersWithApprovals();
-//    }
-//
-//    @Override
-//    public List<TeacherResponseDTO> findTeachers() {
-//        return userRepository.getTeachers();
-//    }
-//
-//    @Override
-//    public List<StudentResponseDTO> findStudents() {
-//        return userRepository.getStudents();
-//    }
-//
-//    @Override
-//    public User approveUser(Long id) {
-//        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
-//        user.setRoleId(3);
-//        user.setApprovalStatus("APPROVED");
-//        return userRepository.save(user);
-//    }
-//
-//    @Override
-//    public void save(User user) {
-//        userRepository.save(user);
-//    }
-//
-//
-//    @Override
-//    public void deleteUser(Long userId) {
-//        User user = userRepository.findById(userId).get();
-//        if (user == null) {
-//            throw new IllegalArgumentException("Không tìm thấy User");
-//        }
-//        user.setIsDeleted(false);
-//        userRepository.save(user);
-//    }
-//
-//    @Override
-//    public Optional<User> findById(Long id) {
-//        return userRepository.findById(id);
-//    }
-//
-//
-//    @Override
-//    public void requestTeacherRole(Long userId) {
-//        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Người dùng không tồn tại với ID: " + userId));
-//        TeacherApproval teacherApproval = teacherApprovalService.findByUserId(userId);
-//        if (teacherApproval != null) {
-//            throw new RuntimeException("Yêu cầu đã được gửi đi trước đó.");
-//        }
-//        TeacherApproval approvalRequest = new TeacherApproval();
-//        approvalRequest.setUser(user);
-//        approvalRequest.setStatus(TeacherApproval.Status.valueOf("PENDING"));
-//        teacherApprovalService.save(approvalRequest);
-//    }
-//
-//    @Override
-//    public void updateUserPassword(Long userId, String newPassword) {
-//        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-//        user.setPassword(newPassword); // Assuming you have a method to hash the password
-//        userRepository.save(user);
-//    }
-//
-//    @Override
-//    public List<UserSearchResponseDTO> findUsersByRolesAndNameOrEmail(Long roleId, String name, String email) {
-//        return userRepository.findUsersByRolesAndNameOrEmail(roleId, name, email);
-//    }
-//}
+package com.codegym.quizappbackendmodule6.service.Impl;
+
+import com.codegym.quizappbackendmodule6.model.TeacherApproval;
+import com.codegym.quizappbackendmodule6.model.User;
+import com.codegym.quizappbackendmodule6.model.dto.StudentResponseDTO;
+import com.codegym.quizappbackendmodule6.model.dto.TeacherResponseDTO;
+import com.codegym.quizappbackendmodule6.model.dto.UserSearchResponseDTO;
+import com.codegym.quizappbackendmodule6.model.dto.UserWithApprovalsProjection;
+import com.codegym.quizappbackendmodule6.repository.UserRepository;
+import com.codegym.quizappbackendmodule6.service.TeacherApprovalService;
+import com.codegym.quizappbackendmodule6.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final TeacherApprovalService teacherApprovalService;
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<UserWithApprovalsProjection> findUsersWithApprovals() {
+        return userRepository.getUsersWithApprovals();
+    }
+
+    @Override
+    public List<TeacherResponseDTO> findTeachers() {
+        return userRepository.getTeachers();
+    }
+
+    @Override
+    public List<StudentResponseDTO> findStudents() {
+        return userRepository.getStudents();
+    }
+
+    @Override
+    public User approveUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setRoleId(3);
+        user.setApprovalStatus("APPROVED");
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId).get();
+        if (user == null) {
+            throw new IllegalArgumentException("Không tìm thấy User");
+        }
+        user.setIsDeleted(false);
+        userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+
+    @Override
+    public void requestTeacherRole(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Người dùng không tồn tại với ID: " + userId));
+        TeacherApproval teacherApproval = teacherApprovalService.findByUserId(userId);
+        if (teacherApproval != null) {
+            throw new RuntimeException("Yêu cầu đã được gửi đi trước đó.");
+        }
+        TeacherApproval approvalRequest = new TeacherApproval();
+        approvalRequest.setUser(user);
+        approvalRequest.setStatus(TeacherApproval.Status.valueOf("PENDING"));
+        teacherApprovalService.save(approvalRequest);
+    }
+
+    @Override
+    public void updateUserPassword(Long userId, String newPassword) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setPassword(newPassword); // Assuming you have a method to hash the password
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<UserSearchResponseDTO> findUsersByRolesAndNameOrEmail(Long roleId, String name, String email) {
+        return userRepository.findUsersByRolesAndNameOrEmail(roleId, name, email);
+    }
+}
