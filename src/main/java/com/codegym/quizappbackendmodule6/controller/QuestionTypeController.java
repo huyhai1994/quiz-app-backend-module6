fcf4ef47ff95c819f21ff8ab4b021e636652328e
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-12 12:21:11 +0700

    Tạo API để lấy loại câu hỏi (một/ nhiều) ok

--- src/main/java/com/codegym/quizappbackendmodule6/controller/QuestionTypeController.java
+++ src/main/java/com/codegym/quizappbackendmodule6/controller/QuestionTypeController.java
@@ -1,9 +1,14 @@
 package com.codegym.quizappbackendmodule6.controller;
 
+import com.codegym.quizappbackendmodule6.model.QuestionType;
+import com.codegym.quizappbackendmodule6.service.QuestionTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class QuestionTypeController {
 
+    private final QuestionTypeService questionTypeService;
+
+    @GetMapping("/get-all")
+    public List<QuestionType> getAllQuestionTypes() {
+        return questionTypeService.findAll();
+    }
 }
