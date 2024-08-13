c02f6ee1138f243860e04e1408e69d77e5b02c6b
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-13 13:50:10 +0700

    gra

--- src/main/java/com/codegym/quizappbackendmodule6/model/dto/QuestionResponse.java
+++ src/main/java/com/codegym/quizappbackendmodule6/model/dto/QuestionResponse.java
@@ -1,6 +1,5 @@
 package com.codegym.quizappbackendmodule6.model.dto;
 
-import com.codegym.quizappbackendmodule6.model.Option;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,4 +7,12 @@ import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionResponse {
+    private Long id;
+    private String questionText;
+    private Set<OptionDTO> options;
+}
