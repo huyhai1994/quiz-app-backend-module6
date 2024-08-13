2be36b320ecdeda74425f37195a30e3dae4eeded
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-13 13:51:37 +0700

    fi

--- src/main/java/com/codegym/quizappbackendmodule6/model/dto/QuestionDTO.java
+++ src/main/java/com/codegym/quizappbackendmodule6/model/dto/QuestionDTO.java
@@ -2,4 +2,10 @@ package com.codegym.quizappbackendmodule6.model.dto;
 
 import java.time.LocalDateTime;
 
-
+public interface QuestionDTO {
+    Long getQuestionId();
+    String getQuestionText();
+    String getCategoryName();
+    String getTypeName();
+    LocalDateTime getTimeCreate();
+}
