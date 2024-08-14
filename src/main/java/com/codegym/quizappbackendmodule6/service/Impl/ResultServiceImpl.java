815692a6c8d4aea88bda96eb1f9556d4baf797b0
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-14 11:50:00 +0700

    moi

--- src/main/java/com/codegym/quizappbackendmodule6/service/Impl/ResultServiceImpl.java
+++ src/main/java/com/codegym/quizappbackendmodule6/service/Impl/ResultServiceImpl.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
