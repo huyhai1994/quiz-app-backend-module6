676048f6906442a8503e6eb6638c6f61aa5ace44
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-14 07:59:15 +0700

    tien

--- src/main/java/com/codegym/quizappbackendmodule6/repository/ResultRepository.java
+++ src/main/java/com/codegym/quizappbackendmodule6/repository/ResultRepository.java
@@ -3,11 +3,15 @@ package com.codegym.quizappbackendmodule6.repository;
 import com.codegym.quizappbackendmodule6.model.Result;
 import com.codegym.quizappbackendmodule6.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ResultRepository extends JpaRepository<Result,Long> {
     List<Result> findByUser(User user);
+    @Query("SELECT COUNT(r) + 1 FROM Result r WHERE r.score > :score")
+    int findRankByScore(@Param("score") Long score);
 
 }
