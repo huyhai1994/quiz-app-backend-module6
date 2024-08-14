dd90a13c0a240e468a7428bcff284452449c37f6
hainam1411 <hainam141199@gmail.com>
2024-08-14 16:45:03 +0700

    sua quizController

--- src/main/java/com/codegym/quizappbackendmodule6/controller/QuizController.java
+++ src/main/java/com/codegym/quizappbackendmodule6/controller/QuizController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/quizzes")
+@RequestMapping("/quiz")
 @RequiredArgsConstructor
 public class QuizController {
     private final QuizService quizService;
